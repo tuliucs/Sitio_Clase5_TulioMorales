6558c6e38e1b8cc83a7e5074cafcad7d0b3c188b
tuliucs <7049153+tuliucs@users.noreply.github.com>
2025-08-09 14:32:46 -0600

    Se carga clase1.java

---
+++ Clase1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+//package hellothread;
+
+/**
+ *
+ * @author Tuliux
+ */
+public class Clase1 {
+
+
+public static void main(String args[]) {
+	System.out.println("hola mundo");
+}
+}
+
